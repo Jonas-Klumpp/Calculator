540699bfcfa13fd28badd5a3dd4a053604e5dca9
Jonas-Klumpp <157389696+Jonas-Klumpp@users.noreply.github.com>
2024-11-07 10:50:01 +0100

    Add files via upload

--- js/style2.js
+++ js/style2.js
@@ -32,7 +32,6 @@ window.onload = function(){
             ConsoleDEBUG ("Main", "Listerner numbers", "userInput", document.getElementById("userInput").innerText);
             
             strUserInput = document.getElementById("userInput").innerText;
-            // document.getElementById("userinput").innerText = strUserInput
 
             ConsoleDEBUG ("Main", "Listerner numbers", "strUserInput", strUserInput);
 
@@ -49,6 +48,8 @@ window.onload = function(){
 
         item.addEventListener("click", function(){
 
+            strUserInput = strUserInput.toString();
+            
             let intStringLenght = strUserInput.length
 
             ConsoleDEBUG("Main","Listerner DELbtn","strUserInput.length",intStringLenght)
@@ -101,7 +102,9 @@ window.onload = function(){
 
         item.addEventListener("click", function(){
 
-            strUserInput = strUserInput
+            document.getElementById("userInput").innerText = document.getElementById("userInput").innerText.concat(item.innerText)
+
+            strUserInput = document.getElementById("userInput").innerText
             
             ConsoleDEBUG ("Main", "Listerner OPSbtn", "userInput", document.getElementById("userInput").innerText);
             ConsoleDEBUG ("Main", "Listerner OPSbtn", "strUserInput", strUserInput);
@@ -112,6 +115,69 @@ window.onload = function(){
 
     };
 
+    const equal = document.getElementById("calc-keys").getElementsByClassName("equal");
+    for (let item of equal) {
+        
+        ConsoleDEBUG("Main", "Listerner equalbtn","-","-")
+
+        item.addEventListener("click", function(){
+
+            document.getElementById("userInput").innerText = document.getElementById("userInput").innerText.concat(item.innerText)
+
+
+
+            strUserInput = eval(strUserInput);
+            document.getElementById("userInput").innerText = strUserInput
+            
+            ConsoleDEBUG ("Main", "Listerner equalbtn", "userInput", document.getElementById("userInput").innerText);
+            ConsoleDEBUG ("Main", "Listerner equalbtn", "strUserInput", strUserInput);
+
+        });
+
+        ConsoleDEBUG ("Main", "Listerner equalbtn", "item", item);
+    };
+
+    const sqared = document.getElementById("calc-keys").getElementsByClassName("btnOpSqr");
+    for (let item of sqared) {
+        
+        ConsoleDEBUG("Main", "Listerner sqaredbtn","-","-")
+
+        item.addEventListener("click", function(){
+
+            strUserInput = eval(strUserInput);
+            ConsoleDEBUG ("Main", "Listerner equalbtn", "EVAL userInput", document.getElementById("userInput").innerText);
+
+            strUserInput = Math.pow(strUserInput,2)
+            document.getElementById("userInput").innerText = strUserInput
+            
+            ConsoleDEBUG ("Main", "Listerner sqaredbtn", "POW userInput", document.getElementById("userInput").innerText);
+            ConsoleDEBUG ("Main", "Listerner sqaredbtn", "strUserInput", strUserInput);
+
+        });
+
+        ConsoleDEBUG ("Main", "Listerner sqaredbtn", "item", item);
+    };
+
+    const fac = document.getElementById("calc-keys").getElementsByClassName("btnOpFac");
+    for (let item of fac) {
+        
+        ConsoleDEBUG("Main", "Listerner facbtn","-","-")
+
+        item.addEventListener("click", function(){
+
+            strUserInput = eval(strUserInput);
+            ConsoleDEBUG ("Main", "Listerner facbtn", "EVAL userInput", document.getElementById("userInput").innerText);
+
+            strUserInput = factorial(strUserInput);
+            document.getElementById("userInput").innerText = strUserInput
+            
+            ConsoleDEBUG ("Main", "Listerner facbtn", "POW userInput", document.getElementById("userInput").innerText);
+            ConsoleDEBUG ("Main", "Listerner facbtn", "strUserInput", strUserInput);
+
+        });
+
+        ConsoleDEBUG ("Main", "Listerner sqaredbtn", "item", item);
+    };
 
 };
 
@@ -120,3 +186,19 @@ function ConsoleDEBUG(strFunction, strText, strVariable, strValue){
     console.log("Function: " + strFunction + " | Hinweis/Pos.: " + strText + " | Variable: "+ strVariable + " | Wert: " + strValue);
 
 };
+
+function factorial(n) {
+
+    ConsoleDEBUG ("factorial", "inputValue", "n", n);
+
+    if (n < 0) return "Factorial for negative numbers is not defined";
+    if (n === 0 || n === 1) return 1;
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result *= i;
+    }
+
+    ConsoleDEBUG ("factorial", "returnValue", "result", result);
+
+    return result;
+}
